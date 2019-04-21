e33d05c88304a870e99da08d3cb5d56ff530268e
chenaxi <1756481939@qq.com>
2019-04-22 00:53:14 +0800

    测试提交009

--- open_GL_ES/app/src/main/java/com/axi/demo/MainActivity.java
+++ open_GL_ES/app/src/main/java/com/axi/demo/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView=findViewById(R.id.text);
+        textView.setText("taylor swift");
 
     }
 }
